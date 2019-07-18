ea97ed17b6100ce1fe4f64663aa6e46f09d322b8
Michael Tryby <tryby.michael@epa.gov>
2019-07-18 16:55:44 -0400

    Update run-nrtest.cmd
    Refactor to eliminate arguments

--- scripts/run-nrtest.cmd
+++ scripts/run-nrtest.cmd
@@ -2,14 +2,22 @@
 ::  run_nrtest.cmd - Runs numerical regression test
 ::
 ::  Date Created: 1/8/2018
-::  Date Updated: 7/16/2018
+::  Date Updated: 7/18/2019
 ::
 ::  Author: Michael E. Tryby
 ::          US EPA - ORD/NRMRL
 ::
+::  Dependencies:
+::    python -m pip install -r requirements.txt
+::
+::  Environment Variables:
+::    TEST_HOME
+::    BUILD_HOME
+::    PLATFORM
+::
 ::  Arguments:
-::    1 - (SUT version identifier)
-::    2 - (SUT build identifier)
+::    1 - (SUT_VERSION)
+::    2 - (SUT_BUILD_ID)
 ::
 
 @echo off
@@ -18,53 +26,58 @@ setlocal EnableDelayedExpansion
 set "SCRIPT_HOME=%~dp0"
 
 :: Check existence
-IF NOT DEFINED REF_BUILD_ID (echo "ERROR: REF_BUILD_ID must be defined" & exit /B 1)
+if not defined REF_BUILD_ID (echo "ERROR: REF_BUILD_ID must be defined" & exit /B 1)
 
-IF [%1]==[] (set SUT_VERSION=
-) ELSE ( set "SUT_VERSION=%~1" )
+if [%1]==[] (set SUT_VERSION=
+) else ( set "SUT_VERSION=%~1" )
 
-IF [%2]==[] ( set "SUT_BUILD_ID=local"
-) ELSE ( set "SUT_BUILD_ID=%~2" )
-
-
-:: determine location of python Scripts folder
-FOR /F "tokens=*" %%G IN ('where python') DO (
-  set PYTHON_DIR=%%~dpG
-  GOTO break_loop_1
-)
-:break_loop_1
-set "NRTEST_SCRIPT_PATH=%PYTHON_DIR%Scripts"
+if [%2]==[] ( set "SUT_BUILD_ID=local"
+) else ( set "SUT_BUILD_ID=%~2" )
 
 
-IF NOT EXIST "apps\epanet-%SUT_BUILD_ID%.json" DO (
+:: check if app config file exists
+if not exist "apps\epanet-%SUT_BUILD_ID%.json" do (
   :: determine SUT executable path
-  FOR /D /R "%BUILD_HOME%" %%a IN (*) DO (
-    IF /i "%%~nxa"=="bin" (
+  for /D /R "%BUILD_HOME%" %%a in (*) do (
+    if /i "%%~nxa"=="bin" (
       set "BIN_HOME=%%a"
-      GOTO break_loop_2
+      goto break_loop_2
     )
   )
   :break_loop_2
   set "SUT_PATH=%BIN_HOME%\Release"
 
-  :: generate json configuration file for software under test
+  :: TODO: determine SUT_VERSION here by calling runepanet.exe
+
+  :: call app config script
   mkdir apps
-  CALL %SCRIPT_HOME%\app-config.cmd %SUT_PATH% %PLATFORM% %SUT_BUILD_ID% %SUT_VERSION%^
+  call %SCRIPT_HOME%\app-config.cmd %SUT_PATH% %PLATFORM% %SUT_BUILD_ID% %SUT_VERSION%^
      > apps\epanet-%SUT_BUILD_ID%.json
 )
 
 
 :: build test list
 set TESTS=
-FOR /F "tokens=*" %%T IN ('dir /b tests') DO ( set "TESTS=!TESTS! tests\%%T" )
+for /F "tokens=*" %%T in ('dir /b tests') do ( set "TESTS=!TESTS! tests\%%T" )
 
 
+:: determine location of python Scripts folder
+for /F "tokens=*" %%G in ('where python') do (
+  set PYTHON_DIR=%%~dpG
+  goto break_loop_1
+)
+:break_loop_1
+set "NRTEST_SCRIPT_PATH=%PYTHON_DIR%Scripts"
+
+
+:: build nrtest execute command
 set NRTEST_EXECUTE_CMD=python %NRTEST_SCRIPT_PATH%\nrtest execute
 set TEST_APP_PATH=apps\epanet-%SUT_BUILD_ID%.json
 set TEST_OUTPUT_PATH=benchmark\epanet-%SUT_BUILD_ID%
 
+:: build nrtest compare command
 set NRTEST_COMPARE_CMD=python %NRTEST_SCRIPT_PATH%\nrtest compare
-set REF_OUTPUT_PATH=benchmark\epanet-%REF_BUILD_ID%
+set REF_OUTPUT_PATH=benchmark\epanet-ref
 set RTOL_VALUE=0.01
 set ATOL_VALUE=0.0
 
@@ -76,6 +89,7 @@ if exist %TEST_OUTPUT_PATH% (
   rmdir /s /q %TEST_OUTPUT_PATH%
 )
 
+:: perform nrtest execute
 echo INFO: Creating SUT %SUT_BUILD_ID% artifacts
 set NRTEST_COMMAND=%NRTEST_EXECUTE_CMD% %TEST_APP_PATH% %TESTS% -o %TEST_OUTPUT_PATH%
 :: if there is an error exit the script with error value 1
@@ -83,6 +97,7 @@ set NRTEST_COMMAND=%NRTEST_EXECUTE_CMD% %TEST_APP_PATH% %TESTS% -o %TEST_OUTPUT_
 
 echo.
 
+:: perform nrtest compare
 echo INFO: Comparing SUT artifacts to REF %REF_BUILD_ID%
 set NRTEST_COMMAND=%NRTEST_COMPARE_CMD% %TEST_OUTPUT_PATH% %REF_OUTPUT_PATH% --rtol %RTOL_VALUE% --atol %ATOL_VALUE% -o benchmark\receipt.json
 %NRTEST_COMMAND%
